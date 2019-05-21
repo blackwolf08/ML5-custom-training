76fc4eaaf09e9ae4f59e752cb9e25af1a073494a
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-21 17:54:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+let mobilenet;
+let classifier;
+let video;
+let label = 'test';
+let obj1Button;
+let obj2Button;
+let obj3Button;
+let testButton;
+
+
+function setup() {
+    createCanvas(320, 270);
+    video = createCapture(VIDEO);
+    video.hide();
+    background(0);
+
+    mobilenet = ml5.featureExtractor('MobileNet', ()=>{
+        console.log('Model ready')
+    });
+    
+    classifier = mobilenet.classification(video, ()=>{
+        console.log('Webcam buffer ready')
+    })
+
+}
+
+function draw() {
+    background(0);
+    image(video, 0, 0, 320, 240);
+    fill(255);
+    textSize(16);
+    text(label, 10, height - 10)
+}
+
+obj1Button = document.getElementById('obj1b');
+obj1Button = document.getElementById('obj2b');
+obj2Button = document.getElementById('obj3b');
+obj3Button = document.getElementById('trainb');
+
+let obj1i = document.getElementById('obj1i');
+let obj2i = document.getElementById('obj2i');
+let obj3i = document.getElementById('obj3i');
+
+
+obj1Button.addEventListener('click', ()=>{
+    classifier.addImage(obj1i.value)
+})
+
+obj2Button.addEventListener('click', ()=>{
+    classifier.addImage(obj2i.value)
+})
+
+obj3Button.addEventListener('click', ()=>{
+    classifier.addImage(obj3i.value)
+})
+
+obj3Button.addEventListener('click', ()=>{
+    classifier.train(loss => {
+        if(loss == null)
+        {
+            classifier.classify(gotResults)
+        }
+    })
+})
+
+function gotResults (err, res) {
+    label = res;
+    classifier.classify(gotResults);
+}
+
+
